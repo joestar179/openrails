78c30101014845858c051ca878f464cc11091db8
Phillip Schlichting <phillip@stlouisrc.com>
2023-08-02 22:10:31 -0500

    Fixing minor style issues, and adding support for both spellings of 'liter'

--- Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
+++ Source/Orts.Simulation/Simulation/RollingStocks/MSTSLocomotive.cs
@@ -5432,6 +5432,7 @@ namespace Orts.Simulation.RollingStocks
                                 break;
 
                             case CABViewControlUnits.LITRES_S:
+                            case CABViewControlUnits.LITERS_S:
                                 data = this.FilteredBrakePipeFlowM3pS * 1000.0f;
                                 break;
 
@@ -5565,7 +5566,6 @@ namespace Orts.Simulation.RollingStocks
 
                                 }
                             }
-
                         }
                         else
                         {
